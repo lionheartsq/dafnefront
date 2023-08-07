ca40cb7851a0d0d6ff13194ef8779139efb40bbe
lionheartsq <lujogarpin78@gmail.com>
2023-08-07 12:34:27 -0500

    Evaluacion service

--- src/app/components/evaluacion/evaluacion.component.ts
+++ src/app/components/evaluacion/evaluacion.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { UtilsService } from 'src/app/services/utils.service';
+import { LoginService } from 'src/app/services/login.service';
+import { EvaluacionService } from 'src/app/services/evaluacion.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-evaluacion',
@@ -7,8 +12,62 @@ import { Router } from '@angular/router';
   styleUrls: ['./evaluacion.component.css']
 })
 export class EvaluacionComponent {
+  idUsuarioCreado: any;
+  @ViewChildren('checkboxes') checkboxesRef!: QueryList<ElementRef>;
+  idIdeaPropia: any;
+  ideaPropia: any;
+  arrayIdeas: any[]=[];
+  arrayCriterios: any[]=[];
+  idCriterioPropio: any;
+  criterioPropio: any;
+  selectedValues: { [key: string]: number } = {};
 
-  constructor(public router: Router) {
+  constructor(public router:Router, private loginService:LoginService, private utilsService:UtilsService, private route: ActivatedRoute, private evaluacionService:EvaluacionService) {
+  }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.idUsuarioCreado = params['id'];
+      //console.log(this.idUsuarioCreado);
+      });
+
+    //this.llamarAPI();
+    this.obtenerIdeasPropio(this.idUsuarioCreado);
+    this.obtenerCriteriosPropio(this.idUsuarioCreado);
+  }
+
+  obtenerIdeasPropio(id:any){
+    this.evaluacionService.getIdeasPropio(id).subscribe(
+      (data) => {
+        //
+        console.log("Data IdeasGen:"+data);
+        for (let dato in data.ideas){
+          this.idIdeaPropia=data.ideas[dato].id;
+          this.ideaPropia=data.ideas[dato].idea;
+          this.arrayIdeas.push({idIdea:this.idIdeaPropia, idea: this.ideaPropia});
+        }
+      },
+      (err) => {
+        console.log(err); // Manejo de errores
+      }
+    );
+  }
+
+  obtenerCriteriosPropio(id:any){
+    this.evaluacionService.getCriteriosPropio(id).subscribe(
+      (data) => {
+        //
+        console.log("Data CriteriosGen:"+data);
+        for (let dato in data.criterios){
+          this.idCriterioPropio=data.criterios[dato].id;
+          this.criterioPropio=data.criterios[dato].criterio;
+          this.arrayCriterios.push({idCriterio:this.idCriterioPropio, criterio: this.criterioPropio});
+        }
+      },
+      (err) => {
+        console.log(err); // Manejo de errores
+      }
+    );
   }
 
   seleccionRoute(){
