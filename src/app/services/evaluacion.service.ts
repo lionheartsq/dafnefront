ed0b060518e64e32e40308479eb3618b3a8137c3
lionheartsq <lujogarpin78@gmail.com>
2023-08-18 04:07:36 -0500

    Matriz component

--- src/app/services/evaluacion.service.ts
+++ src/app/services/evaluacion.service.ts
@@ -14,13 +14,9 @@ export class EvaluacionService {
 
   //private urlBase='http://127.0.0.1:8000';
 
-  private endpointGetIdeasCriterios='api/auth/relacion_ideas_criterios/relacionideascriterios';
+  private endpointGetideas='api/auth/usuario_ideas/usuarioideas';
 
-  private endpointCountRelacion='api/auth/relacion_ideas_criterios/countrelacionideascriterios';
-
-  private endpointSaveIdeas='api/auth/relacion_ideas_criterios/store';
-
-  private endpointUpdateRelacion='api/auth/relacion_ideas_criterios/updaterelacionideascriterios';
+  private endpointGetCriterios='api/auth/usuario_criterios/usuariocriterios';
 
   private endpointStore='api/auth/criterios_evaluacion/store';
 
@@ -29,21 +25,12 @@ export class EvaluacionService {
   constructor(private httpClient: HttpClient) { }
 
 
-  public getIdeasCriterios(id:any): Observable<any> {
-    return this.httpClient.get(`${this.urlBase}/${this.endpointGetIdeasCriterios}/${id}`);
+  public getIdeasPropio(id:any): Observable<any> {
+    return this.httpClient.get(`${this.urlBase}/${this.endpointGetideas}/${id}`);
   }
 
-  public countRelacionPropio(id:any): Observable<any> {
-    return this.httpClient.get(`${this.urlBase}/${this.endpointCountRelacion}/${id}`);
-  }
-
-  public crearRelacion(user: any): Observable<any>{
-    return this.httpClient.post(`${this.urlBase}/${this.endpointSaveIdeas}`, user)
-  }
-
-  public enviarValor(data: any): Observable<any> {
-    const url = `${this.urlBase}/${this.endpointUpdateRelacion}`; // Reemplaza con la URL de tu endpoint
-    return this.httpClient.post(url, data);
+  public getCriteriosPropio(id:any): Observable<any> {
+    return this.httpClient.get(`${this.urlBase}/${this.endpointGetCriterios}/${id}`);
   }
 
   crearRegistro(user: any): Observable<any>{
