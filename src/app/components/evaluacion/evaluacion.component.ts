c084212806e2a32095fc8e2e1fd76f449f063de7
lionheartsq <lujogarpin78@gmail.com>
2023-08-21 23:54:38 -0500

    Arreglo Matriz

--- src/app/components/evaluacion/evaluacion.component.ts
+++ src/app/components/evaluacion/evaluacion.component.ts
@@ -1,13 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ElementRef, QueryList, ViewChildren } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { UtilsService } from 'src/app/services/utils.service';
-import { LoginService } from 'src/app/services/login.service';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { EvaluacionService } from 'src/app/services/evaluacion.service';
-import Swal from 'sweetalert2';
-import { IdeasService } from '../../services/ideas.service';
-import { CriteriosService } from 'src/app/services/criterios.service';
-import { data } from 'jquery';
 
 @Component({
   selector: 'app-evaluacion',
@@ -16,100 +9,75 @@ import { data } from 'jquery';
 })
 export class EvaluacionComponent implements OnInit {
   idUsuarioCreado: any;
-  @ViewChildren('checkboxes') checkboxesRef!: QueryList<ElementRef>;
-  idIdeaPropia: any;
-  ideaPropia: any;
-  arrayIdeas: any[]=[];
-  arrayCriterios: any[]=[];
-  idCriterioPropio: any;
-  criterioPropio: any;
-  datoVuelta: number=0;
+  arrayIdeas: any[] = [];
+  arrayCriterios: any[] = [];
   criteriosIdeas: { criterioId: any, ideaId: any, idUsuario: any, porcentaje: number }[] = [];
-  porcentaje: number=0;
 
-  constructor(public router:Router, private loginService:LoginService, private utilsService:UtilsService, private route: ActivatedRoute, private evaluacionService:EvaluacionService) {
-  }
+  constructor(public router: Router, private route: ActivatedRoute, private evaluacionService: EvaluacionService) {}
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       this.idUsuarioCreado = params['id'];
-      //console.log(this.idUsuarioCreado);
-      });
+    });
 
-    //this.llamarAPI();
     this.obtenerIdeasPropio(this.idUsuarioCreado);
     this.obtenerCriteriosPropio(this.idUsuarioCreado);
-    console.log("Data IdeasArray:"+this.arrayIdeas.length);
-    console.log("Data CriteriosArray:"+this.arrayCriterios.length);
   }
 
-  obtenerIdeasPropio(id:any){
+  obtenerIdeasPropio(id: any) {
     this.evaluacionService.getIdeasPropio(id).subscribe(
       (data) => {
-        //
-        this.arrayIdeas=data.ideas;
+        this.arrayIdeas = data.ideas;
+        this.initializeIdeaValues();
       },
       (err) => {
-        console.log(err); // Manejo de errores
+        console.log(err);
       }
     );
   }
 
-  obtenerCriteriosPropio(id:any){
+  obtenerCriteriosPropio(id: any) {
     this.evaluacionService.getCriteriosPropio(id).subscribe(
       (data) => {
-        //
-        this.arrayCriterios=data.criterios;
+        this.arrayCriterios = data.criterios;
       },
       (err) => {
-        console.log(err); // Manejo de errores
+        console.log(err);
       }
     );
+  }
+
+  initializeIdeaValues() {
+    this.arrayIdeas.forEach(idea => {
+      idea.valores = {}; // Inicializar un objeto para almacenar los valores de los criterios
+    });
   }
 
   seleccionSave() {
-    // Create an array to store the relationships between criterios, ideas, and values
     this.criteriosIdeas = [];
 
-    // Loop through the criterios and their associated ideas to capture the relationships and values
     this.arrayCriterios.forEach(criterio => {
       this.arrayIdeas.forEach(idea => {
-        const inputValue = this.getInputValue(criterio.id, idea.id);
-        if (inputValue !== null) {
-          const valor = parseInt(inputValue, 10);
-          if (!isNaN(valor) && valor >= 1 && valor <= 3) {
+        const inputValue = idea.valores[criterio.id];
+        if (inputValue !== undefined && inputValue >= 1 && inputValue <= 3) {
+          const porcentaje = inputValue === 1 ? 50 : (inputValue === 2 ? 33 : 17);
+          this.criteriosIdeas.push({ criterioId: criterio.id, ideaId: idea.id, idUsuario: this.idUsuarioCreado, porcentaje: porcentaje });
 
-            if(valor==1){this.porcentaje=50}
-            else if(valor==2){this.porcentaje=33}
-            else if(valor==3){this.porcentaje=17}
+          const usuario = { idCriterio: criterio.id, idIdea: idea.id, idUsuario: this.idUsuarioCreado, porcentaje: porcentaje };
 
-            this.criteriosIdeas.push({ criterioId: criterio.id, ideaId: idea.id, idUsuario:this.idUsuarioCreado, porcentaje: this.porcentaje });
-
-            const usuario = {idCriterio: criterio.id, idIdea: idea.id, idUsuario:this.idUsuarioCreado, porcentaje: this.porcentaje};
-
-            this.evaluacionService.crearRegistro(usuario).subscribe( (data)=>{
+          this.evaluacionService.crearRegistro(usuario).subscribe(
+            data => {
               console.log(usuario);
-          }, (err) => {
-            //debugger
-            console.log(err);
-          });
-
-          }
+            },
+            err => {
+              console.log(err);
+            }
+          );
         }
       });
     });
 
-        console.log(this.criteriosIdeas); // This will show the collected data
-        this.router.navigate(['matriz'], { queryParams: { id: this.idUsuarioCreado} } );
+    console.log(this.criteriosIdeas);
+    this.router.navigate(['matriz'], { queryParams: { id: this.idUsuarioCreado } });
   }
-
-  private getInputValue(criterioId: any, ideaId: any): string | null {
-    const inputElement = document.getElementById(`${criterioId}_${ideaId}`) as HTMLInputElement;
-    return inputElement ? inputElement.value : null;
-  }
-
-  seleccionRoute(){
-    this.router.navigate(['seleccion']);
-  }
-
 }
