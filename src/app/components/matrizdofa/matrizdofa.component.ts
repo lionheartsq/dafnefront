001f465bab2699460ce87ccf646868a803a9a445
lionheartsq <lujogarpin78@gmail.com>
2023-10-20 01:09:49 -0500

    Push

--- src/app/components/matrizdofa/matrizdofa.component.ts
+++ src/app/components/matrizdofa/matrizdofa.component.ts
@@ -14,17 +14,37 @@ import Swal from 'sweetalert2';
 })
 export class MatrizdofaComponent {
   idUsuarioCreado: any;
-  debilidades: any="";
-  oportunidades: any="";
-  fortalezas: any="";
-  amenazas: any="";
-  avanced: any="0";
+  debilidades1: any="";
+  debilidades2: any="";
+  debilidades3: any="";
+  debilidades4: any="";
+  oportunidades1: any="";
+  oportunidades2: any="";
+  oportunidades3: any="";
+  oportunidades4: any="";
+  fortalezas1: any="";
+  fortalezas2: any="";
+  fortalezas3: any="";
+  fortalezas4: any="";
+  amenazas1: any="";
+  amenazas2: any="";
+  amenazas3: any="";
+  amenazas4: any="";
+  avanced: number=0;
   arrayDofa: any;
-  avanceo: any="";
+  avanceo: number=0;
   idDofa: any="";
-  avancef: any="";
-  avancea: any="";
+  avancef: number=0;
+  avancea: number=0;
   valorescarga: string="";
+  debilidadesConsolidado: string="";
+  oportunidadesConsolidado: string="";
+  fortalezasConsolidado: string="";
+  amenazasConsolidado: string="";
+  colord: string="";
+  coloro: string="";
+  colorf: string="";
+  colora: string="";
 
   constructor(public router:Router, private loginService:LoginService, private utilsService:UtilsService, private route: ActivatedRoute, private dofaService:DofaService) {}
 
@@ -44,16 +64,68 @@ export class MatrizdofaComponent {
         this.arrayDofa=data.matriz_dofa;
         for (let dato in this.arrayDofa){
           this.idDofa=this.arrayDofa[dato].id;
-          this.debilidades=this.arrayDofa[dato].debilidades;
-          this.oportunidades=this.arrayDofa[dato].oportunidades;
-          this.fortalezas=this.arrayDofa[dato].fortalezas;
-          this.amenazas=this.arrayDofa[dato].amenazas;
+          this.debilidades1=this.arrayDofa[dato].debilidades1;
+          this.oportunidades1=this.arrayDofa[dato].oportunidades1;
+          this.fortalezas1=this.arrayDofa[dato].fortalezas1;
+          this.amenazas1=this.arrayDofa[dato].amenazas1;
+          this.debilidades2=this.arrayDofa[dato].debilidades2;
+          this.oportunidades2=this.arrayDofa[dato].oportunidades2;
+          this.fortalezas2=this.arrayDofa[dato].fortalezas2;
+          this.amenazas2=this.arrayDofa[dato].amenazas2;
+          this.debilidades3=this.arrayDofa[dato].debilidades3;
+          this.oportunidades3=this.arrayDofa[dato].oportunidades3;
+          this.fortalezas3=this.arrayDofa[dato].fortalezas3;
+          this.amenazas3=this.arrayDofa[dato].amenazas3;
+          this.debilidades4=this.arrayDofa[dato].debilidades4;
+          this.oportunidades4=this.arrayDofa[dato].oportunidades4;
+          this.fortalezas4=this.arrayDofa[dato].fortalezas4;
+          this.amenazas4=this.arrayDofa[dato].amenazas4;
           this.avanced=this.arrayDofa[dato].avanced;
           this.avanceo=this.arrayDofa[dato].avanceo;
           this.avancef=this.arrayDofa[dato].avancef;
           this.avancea=this.arrayDofa[dato].avancea;
         }
         //console.log("Actual idEmpresa: "+this.idEmpresa);
+        this.debilidadesConsolidado=this.debilidades1+"\n"+this.debilidades2+"\n"+this.debilidades3+"\n"+this.debilidades4;
+        this.oportunidadesConsolidado=this.oportunidades1+"\n"+this.oportunidades2+"\n"+this.oportunidades3+"\n"+this.oportunidades4;
+        this.fortalezasConsolidado=this.fortalezas1+"\n"+this.fortalezas2+"\n"+this.fortalezas3+"\n"+this.fortalezas4;
+        this.amenazasConsolidado=this.amenazas1+"\n"+this.amenazas2+"\n"+this.amenazas3+"\n"+this.amenazas4;
+
+        switch(this.avanced){
+          case 0: this.colord="red";
+            break;
+          case 1: this.colord="orange";
+            break;
+          case 2: this.colord="green";
+            break;
+        }
+
+        switch(this.avanceo){
+          case 0: this.coloro="red";
+            break;
+          case 1: this.coloro="orange";
+            break;
+          case 2: this.coloro="green";
+            break;
+        }
+
+        switch(this.avancef){
+          case 0: this.colorf="red";
+            break;
+          case 1: this.colorf="orange";
+            break;
+          case 2: this.colorf="green";
+            break;
+        }
+
+        switch(this.avancea){
+          case 0: this.colora="red";
+            break;
+          case 1: this.colora="orange";
+            break;
+          case 2: this.colora="green";
+            break;
+        }
       },
       (err) => {
         console.log(err); // Manejo de errores
@@ -62,113 +134,85 @@ export class MatrizdofaComponent {
   }
 
   uploadDebilidades() {
-     this.openPopup("debilidades");
+     //this.openPopup("debilidades");
+     this.router.navigate(['debilidades'], { queryParams: { id: this.idUsuarioCreado} } );
   }
 
   uploadOportunidades() {
-    this.openPopup("oportunidades");
+    //this.openPopup("oportunidades");
+    this.router.navigate(['oportunidades'], { queryParams: { id: this.idUsuarioCreado} } );
   }
 
   uploadFortalezas() {
-    this.openPopup("fortalezas");
+    //this.openPopup("fortalezas");
+    this.router.navigate(['fortalezas'], { queryParams: { id: this.idUsuarioCreado} } );
   }
 
   uploadAmenazas() {
-    this.openPopup("amenazas");
+    //this.openPopup("amenazas");
+    this.router.navigate(['amenazas'], { queryParams: { id: this.idUsuarioCreado} } );
   }
 
 
-  openPopup(mensaje:String) {
-    switch(mensaje) {
-      case "debilidades": {
-         this.valorescarga=this.debilidades;
-         break;
-      }
-      case "oportunidades": {
-        this.valorescarga=this.oportunidades;
-        break;
-      }
-      case "fortalezas": {
-        this.valorescarga=this.fortalezas;
-        break;
-     }
-     case "amenazas": {
-        this.valorescarga=this.amenazas;
-        break;
-     }
-      default: {
-         //statements;
-         break;
-      }
-   }
-    Swal.fire({
-      title: 'Ingresa las '+mensaje+' a registrar:',
-      html:
-        '<input type="text" id="criterionuevo" class="swal2-input" placeholder="Valor a ingresar" value="'+this.valorescarga+'" required>',
-      showCancelButton: true,
-      confirmButtonText: 'Guardar',
-      preConfirm: () => {
-        // Obtener el valor del input
-        const criterionuevo = (document.getElementById('criterionuevo') as HTMLInputElement).value;
-        switch(mensaje) {
-          case "debilidades": {
-            this.debilidades=criterionuevo;
-             break;
-          }
-          case "oportunidades": {
-            this.oportunidades=criterionuevo;
-            break;
-          }
-          case "fortalezas": {
-            this.fortalezas=criterionuevo;
-            break;
-         }
-         case "amenazas": {
-            this.amenazas=criterionuevo;
-            break;
-         }
-          default: {
-             //statements;
-             break;
-          }
-       }
-        // Realizar aquí las acciones necesarias con el valor ingresado
-        const varMensaje = {id:this.idDofa, idUsuario:this.idUsuarioCreado, debilidades:this.debilidades, oportunidades:this.oportunidades, fortalezas:this.fortalezas, amenazas:this.amenazas, avanced:this.avanced, avanceo:this.avanceo, avancef:this.avancef, avancea:this.avancea};
-        console.log("Var "+mensaje+": "+varMensaje);
+  openPopup() {
+      // Crea el contenido HTML que deseas mostrar en el cuadro de diálogo
+      const tablaHTML = `
+      <table class="table">
+        <tr>
+          <td>
+            <p>D</p>
+            <mat-card class="example-card" [style.background-color]="colord">
+              <mat-card-content>`+
+                this.debilidadesConsolidado
+              +`</mat-card-content>
+            </mat-card>
+          </td>
+          <td>
+            <p>O</p>
+            <mat-card class="example-card" [style.background-color]="coloro">
+              <mat-card-content>`+
+                this.oportunidadesConsolidado
+              +`</mat-card-content>
+            </mat-card>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>F</p>
+            <mat-card class="example-card" [style.background-color]="colorf">
+              <mat-card-content>`+
+                this.fortalezasConsolidado
+              +`</mat-card-content>
+            </mat-card>
+          </td>
+          <td>
+            <p>A</p>
+            <mat-card class="example-card" [style.background-color]="colora">
+              <mat-card-content>`+
+                this.amenazasConsolidado
+              +`</mat-card-content>
+            </mat-card>
+          </td>
+        </tr>
+      </table>
+      `;
 
-        this.dofaService.enviarValor(varMensaje).subscribe( (data)=>{
-          Swal.fire(
-            {
-              icon: 'success',
-              title: 'Solicitud enviada',
-              text: mensaje+' empresa cargadas correctamente',
-              footer: data.message
-            }
-          ).then(() => {
-            //this.router.navigate(['resumen'], { queryParams: { id: this.idUsuarioCreado} } );
-            //
-            window.location.reload();
-          });
-        }, (err) => {
-          //debugger
-          Swal.fire(
-            {
-              icon: 'error',
-              title: 'Error al crear',
-              html: 'Por favor verifique los datos e intente nuevamente',
-              footer: 'No se ha podido completar el registro'
-            }
-          )
-        });
-      }
-    })
+      // Muestra el cuadro de diálogo SweetAlert 2 con el contenido HTML
+      Swal.fire({
+      title: 'Matriz Dofa',
+      html: tablaHTML,
+      showCloseButton: true,
+      showCancelButton: false,
+      showConfirmButton: false,
+      });
+    }
+
+  matrizRoute() {
+    this.openPopup();
   }
 
-  amenazasRoute(){
-    this.router.navigate(['amenazas']);
-  }
-
-  homeRoute(){
-    this.router.navigate(['home']);
+  canvasRoute() {
+    //this.openPopup("fortalezas");
+    this.router.navigate(['canvas'], { queryParams: { id: this.idUsuarioCreado} } );
   }
 }
